5dc56e87ef95729dd8fcf57940b85d0612e1c893
Biki <biki.hamzah@gmail.com>
2021-05-04 18:22:59 +0700

    callback and promise

---
+++ Tugas-7-Asynchronous/index.js
@@ -0,0 +1,15 @@
+var readBooks = require('./callback.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+
+function log(sisaWaktu) {
+    return console.log(sisaWaktu);
+
+}
+
+console.log(books.forEach( book=> readBooks(10000, book, log) ));
